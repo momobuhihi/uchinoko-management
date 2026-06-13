6c88905e6921f49483fb64d95afda13cb1af9e2c
momochi <niwatorigomi@gmail.com>
2026-06-13 10:07:17 +0900

    Initial project setup

---
+++ src/main/java/com/uchinoko/uchinokomanagement/UchinokoManagementApplication.java
@@ -0,0 +1,13 @@
+package com.uchinoko.uchinokomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UchinokoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UchinokoManagementApplication.class, args);
+	}
+
+}
